e1f993690ccbe1f71dc3191b97c3348e40f1e1dd
Neil Boyd <NeilBoyd@gmail.com>
2022-11-08 07:00:56 +0100

    create a test project

---
+++ src/Teapot.Web.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Teapot.Web.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
