ad37e68d88c79febcd0ab5a60d33abc91aa26111
Neil Boyd <NeilBoyd@gmail.com>
2022-11-16 07:17:56 +0100

    setup test

--- src/Teapot.Web.Tests/UnitTests/CustomHttpStatusCodeResultTests.cs
+++ src/Teapot.Web.Tests/UnitTests/CustomHttpStatusCodeResultTests.cs
@@ -1,10 +1,46 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
 using Teapot.Web.Models;
 
 namespace Teapot.Web.Tests.UnitTests
 {
     public class CustomHttpStatusCodeResultTests
     {
+        private Mock<HttpContext> _mockHttpContext;
+        private Mock<ActionContext> _mockActionContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            var logger = new Mock<ILogger>();
+            var loggerFactory = new Mock<ILoggerFactory>();
+            var serviceProvider = new Mock<IServiceProvider>();
+            loggerFactory.Setup(x => x.CreateLogger(It.IsAny<string>())).Returns(logger.Object);
+            serviceProvider.Setup(x => x.GetService(typeof(ILogger))).Returns(logger.Object);
+            serviceProvider.Setup(x => x.GetService(typeof(ILoggerFactory))).Returns(loggerFactory.Object);
+
+            var headerDictionary = new HeaderDictionary();
+            var httpRequest = new Mock<HttpRequest>();
+            var httpResponse = new Mock<HttpResponse>();
+            var features = new Mock<IFeatureCollection>();
+            httpRequest.SetupGet(x => x.Headers).Returns(headerDictionary);
+
+            _mockHttpContext = new Mock<HttpContext>();
+            _mockHttpContext.Setup(x => x.Features).Returns(features.Object);
+            _mockHttpContext.Setup(x => x.Request).Returns(httpRequest.Object);
+            _mockHttpContext.Setup(x => x.Response).Returns(httpResponse.Object);
+            _mockHttpContext.Setup(x => x.RequestServices).Returns(serviceProvider.Object);
+
+            var routeData = new Mock<RouteData>();
+            var actionDescriptor = new Mock<ActionDescriptor>();
+
+            _mockActionContext = new Mock<ActionContext>(_mockHttpContext.Object, routeData.Object, actionDescriptor.Object);
+        }
+
         [TestCaseSource(typeof(ExtendedHttpStatusCodes), nameof(ExtendedHttpStatusCodes.StatusCodes))]
         public async Task ResponseStatusCode_IsCorrect(ExtendedHttpStatusCode httpStatusCode)
         {
@@ -12,13 +48,12 @@ namespace Teapot.Web.Tests.UnitTests
             {
                 Description = httpStatusCode.Message
             };
-            var mockContext = new Mock<ActionContext>();
 
             var target = new CustomHttpStatusCodeResult(httpStatusCode.Code, statusCodeResult);
 
-            await target.ExecuteResultAsync(mockContext.Object);
+            await target.ExecuteResultAsync(_mockActionContext.Object);
 
-            mockContext.VerifySet(x => x.HttpContext.Response.StatusCode = httpStatusCode.Code);
+            _mockHttpContext.VerifySet(x => x.Response.StatusCode = httpStatusCode.Code);
         }
     }
 }
