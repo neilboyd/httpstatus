54acfad7342d94f75d3e4d55416f4745b1c236fb
Neil Boyd <NeilBoyd@gmail.com>
2022-11-17 17:34:18 +0100

    convert to file-scoped namespace

--- src/Teapot.Web.Tests/ExtendedHttpStatusCodes.cs
+++ src/Teapot.Web.Tests/ExtendedHttpStatusCodes.cs
@@ -1,18 +1,17 @@
-﻿namespace Teapot.Web.Tests
+﻿namespace Teapot.Web.Tests;
+
+public class ExtendedHttpStatusCodes
 {
-    public class ExtendedHttpStatusCodes
-    {
-        public static IEnumerable<ExtendedHttpStatusCode> StatusCodes =>
-            new[] {
-                new ExtendedHttpStatusCode(300, "Multiple Choices"),
-                new ExtendedHttpStatusCode(301, "Moved Permanently"),
-                new ExtendedHttpStatusCode(302, "Found"),
-                new ExtendedHttpStatusCode(303, "See Other"),
-                new ExtendedHttpStatusCode(306, "Switch Proxy"),
-                new ExtendedHttpStatusCode(418, "I'm a teapot"),
-                new ExtendedHttpStatusCode(424, "424 Unknown Code")
-            }
-            .Union(Enum.GetValues<HttpStatusCode>()
-                       .Select(x => new ExtendedHttpStatusCode((int)x, x.ToString())));
-    }
+    public static IEnumerable<ExtendedHttpStatusCode> StatusCodes =>
+        new[] {
+            new ExtendedHttpStatusCode(300, "Multiple Choices"),
+            new ExtendedHttpStatusCode(301, "Moved Permanently"),
+            new ExtendedHttpStatusCode(302, "Found"),
+            new ExtendedHttpStatusCode(303, "See Other"),
+            new ExtendedHttpStatusCode(306, "Switch Proxy"),
+            new ExtendedHttpStatusCode(418, "I'm a teapot"),
+            new ExtendedHttpStatusCode(424, "424 Unknown Code")
+        }
+        .Union(Enum.GetValues<HttpStatusCode>()
+                   .Select(x => new ExtendedHttpStatusCode((int)x, x.ToString())));
 }
--- src/Teapot.Web.Tests/IntegrationTests/IntegrationTests.cs
+++ src/Teapot.Web.Tests/IntegrationTests/IntegrationTests.cs
@@ -1,27 +1,26 @@
-﻿namespace Teapot.Web.Tests.IntegrationTests
+﻿namespace Teapot.Web.Tests.IntegrationTests;
+
+[Category("Integration")]
+public class IntegrationTests
 {
-    [Category("Integration")]
-    public class IntegrationTests
+    private static readonly HttpClient _httpClient = new(new HttpClientHandler { AllowAutoRedirect = false });
+    private Uri _uri;
+
+    [SetUp]
+    public void Setup()
     {
-        private static readonly HttpClient _httpClient = new(new HttpClientHandler { AllowAutoRedirect = false });
-        private Uri _uri;
+        var appName = Environment.GetEnvironmentVariable("AZURE_WEBAPP_NAME");
+        Assert.That(appName, Is.Not.Empty);
+        _uri = new Uri($"https://{appName}.azurewebsites.net");
+    }
 
-        [SetUp]
-        public void Setup()
-        {
-            var appName = Environment.GetEnvironmentVariable("AZURE_WEBAPP_NAME");
-            Assert.That(appName, Is.Not.Empty);
-            _uri = new Uri($"https://{appName}.azurewebsites.net");
-        }
-
-        [TestCaseSource(typeof(ExtendedHttpStatusCodes), nameof(ExtendedHttpStatusCodes.StatusCodes))]
-        public async Task Response_Is_Correct([Values] ExtendedHttpStatusCode httpStatusCode)
-        {
-            var uri = new Uri(_uri, $"/{httpStatusCode.Code}");
-            using var response = await _httpClient.GetAsync(uri);
-            Assert.That((int)response.StatusCode, Is.EqualTo(httpStatusCode.Code));
-            var body = await response.Content.ReadAsStringAsync();
-            Assert.That(body, Is.EqualTo(httpStatusCode.ToString()).OnlyAlphanumericIgnoreCase());
-        }
+    [TestCaseSource(typeof(ExtendedHttpStatusCodes), nameof(ExtendedHttpStatusCodes.StatusCodes))]
+    public async Task Response_Is_Correct([Values] ExtendedHttpStatusCode httpStatusCode)
+    {
+        var uri = new Uri(_uri, $"/{httpStatusCode.Code}");
+        using var response = await _httpClient.GetAsync(uri);
+        Assert.That((int)response.StatusCode, Is.EqualTo(httpStatusCode.Code));
+        var body = await response.Content.ReadAsStringAsync();
+        Assert.That(body, Is.EqualTo(httpStatusCode.ToString()).OnlyAlphanumericIgnoreCase());
     }
 }
