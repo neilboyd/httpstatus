e51d3516a04bbe41f1553210f407d7526b90116c
Neil Boyd <NeilBoyd@gmail.com>
2022-11-20 13:08:17 +0100

    add new non-standard codes

--- src/Teapot.Web.Tests/ExtendedHttpStatusCodes.cs
+++ src/Teapot.Web.Tests/ExtendedHttpStatusCodes.cs
@@ -11,10 +11,21 @@ public class ExtendedHttpStatusCodes
             new ExtendedHttpStatusCode(303, "See Other"),
             new ExtendedHttpStatusCode(306, "Switch Proxy"),
             new ExtendedHttpStatusCode(418, "I'm a teapot"),
-            new ExtendedHttpStatusCode(425, "Too Early")
-        };
-
-    private static readonly ExtendedHttpStatusCode[] CloudflareStatusCodes = new[] {
+            new ExtendedHttpStatusCode(419, "CSRF Token Missong or Expired"),
+            new ExtendedHttpStatusCode(420, "Enhance Your Calm"),
+            new ExtendedHttpStatusCode(425, "Too Early"),
+            new ExtendedHttpStatusCode(440, "Login Time-out"),
+            new ExtendedHttpStatusCode(444, "No Response"),
+            new ExtendedHttpStatusCode(449, "Retry With"),
+            new ExtendedHttpStatusCode(450, "Blocked by Windows Parental Controls"),
+            new ExtendedHttpStatusCode(460, "Client closed the connection with AWS Elastic Load Balancer"),
+            new ExtendedHttpStatusCode(463, "The load balancer received an X-Forwarded-For request header with more than 30 IP addresses"),
+            new ExtendedHttpStatusCode(494, "Request header too large"),
+            new ExtendedHttpStatusCode(495, "SSL Certificate Error"),
+            new ExtendedHttpStatusCode(496, "SSL Certificate Required"),
+            new ExtendedHttpStatusCode(497, "HTTP Request Sent to HTTPS Port"),
+            new ExtendedHttpStatusCode(498, "Invalid Token (Esri)"),
+            new ExtendedHttpStatusCode(499, "Client Closed Request"),
             new ExtendedHttpStatusCode(520, "Web Server Returned an Unknown Error"),
             new ExtendedHttpStatusCode(521, "Web Server Is Down"),
             new ExtendedHttpStatusCode(522, "Connection Timed out"),
@@ -23,7 +34,8 @@ public class ExtendedHttpStatusCodes
             new ExtendedHttpStatusCode(525, "SSL Handshake Failed"),
             new ExtendedHttpStatusCode(526, "Invalid SSL Certificate"),
             new ExtendedHttpStatusCode(527, "Railgun Error"),
-            new ExtendedHttpStatusCode(530, "Origin DNS Error")
+            new ExtendedHttpStatusCode(530, "Origin DNS Error"),
+            new ExtendedHttpStatusCode(561, "Unauthorized (AWS Elastic Load Balancer)")
         };
 
     private static readonly HttpStatusCode[] OfficialStatusCodes = Enum.GetValues<HttpStatusCode>();
@@ -45,12 +57,10 @@ public class ExtendedHttpStatusCodes
 
     public static IEnumerable<ExtendedHttpStatusCode> StatusCodesAll =>
         NonStandard
-        .Union(CloudflareStatusCodes)
         .Union(OfficialStatusCodes.Select(Map));
 
     public static IEnumerable<ExtendedHttpStatusCode> StatusCodesWithContent =>
         NonStandard
-        .Union(CloudflareStatusCodes)
         .Union(OfficialStatusCodes
                    .Except(NoContentStatusCodes)
                    .Except(ServerErrorStatusCodes)
