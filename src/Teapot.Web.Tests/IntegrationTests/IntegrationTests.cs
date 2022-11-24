05eb3263bf168b8cbad160b92cb56aac539e64fb
Neil Boyd <NeilBoyd@gmail.com>
2022-11-24 06:56:58 +0100

    Use Microsoft.AspNetCore.Mvc.Testing

--- src/Teapot.Web.Tests/IntegrationTests/IntegrationTests.cs
+++ src/Teapot.Web.Tests/IntegrationTests/IntegrationTests.cs
@@ -1,23 +1,23 @@
-﻿namespace Teapot.Web.Tests.IntegrationTests;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Teapot.Web.Tests.IntegrationTests;
 
 [Category("Integration")]
 public class IntegrationTests
 {
-    private static readonly HttpClient _httpClient = new(new HttpClientHandler { AllowAutoRedirect = false });
-    private Uri _uri;
+    private static HttpClient _httpClient;
 
     [SetUp]
     public void Setup()
     {
-        var appName = Environment.GetEnvironmentVariable("AZURE_WEBAPP_NAME");
-        Assert.That(appName, Is.Not.Null.And.Not.Empty);
-        _uri = new Uri($"https://{appName}.azurewebsites.net");
+        var webAppFactory = new WebApplicationFactory<Program>();
+        _httpClient = webAppFactory.CreateDefaultClient();
     }
 
     [TestCaseSource(typeof(ExtendedHttpStatusCodes), nameof(ExtendedHttpStatusCodes.StatusCodesWithContent))]
     public async Task ResponseWithContent([Values] ExtendedHttpStatusCode httpStatusCode)
     {
-        var uri = new Uri(_uri, $"/{httpStatusCode.Code}");
+        var uri = $"/{httpStatusCode.Code}";
         using var response = await _httpClient.GetAsync(uri);
         Assert.That((int)response.StatusCode, Is.EqualTo(httpStatusCode.Code));
         var body = await response.Content.ReadAsStringAsync();
@@ -27,7 +27,7 @@ public class IntegrationTests
     [TestCaseSource(typeof(ExtendedHttpStatusCodes), nameof(ExtendedHttpStatusCodes.StatusCodesNoContent))]
     public async Task ResponseNoContent([Values] ExtendedHttpStatusCode httpStatusCode)
     {
-        var uri = new Uri(_uri, $"/{httpStatusCode.Code}");
+        var uri = $"/{httpStatusCode.Code}";
         using var response = await _httpClient.GetAsync(uri);
         Assert.That((int)response.StatusCode, Is.EqualTo(httpStatusCode.Code));
         var body = await response.Content.ReadAsStringAsync();
@@ -37,7 +37,7 @@ public class IntegrationTests
     [TestCaseSource(typeof(ExtendedHttpStatusCodes), nameof(ExtendedHttpStatusCodes.StatusCodesServerError))]
     public async Task ResponseServerError([Values] ExtendedHttpStatusCode httpStatusCode)
     {
-        var uri = new Uri(_uri, $"/{httpStatusCode.Code}");
+        var uri = $"/{httpStatusCode.Code}";
         using var response = await _httpClient.GetAsync(uri);
         Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadGateway));
         var body = await response.Content.ReadAsStringAsync();
