6aff13727eae44aa3c56a92698ebec48949250bf
Neil Boyd <NeilBoyd@gmail.com>
2023-02-03 07:54:20 +0100

    fix warnings

--- src/Teapot.Web/RandomSequenceGenerator.cs
+++ src/Teapot.Web/RandomSequenceGenerator.cs
@@ -6,27 +6,24 @@ namespace Teapot.Web;
 public class RandomSequenceGenerator
 {
     private readonly Random _random = new();
-    private int[] Range { get; set; }
+    private int[] Range { get; set; } = Array.Empty<int>();
 
     public static bool TryParse(string range, out RandomSequenceGenerator generator)
     {
+        generator = new RandomSequenceGenerator();
         try
         {
-            generator = new RandomSequenceGenerator
-            {
-                // copied from https://stackoverflow.com/a/37213725/260221
-                Range = range.Split(',')
-                             .Select(x => x.Split('-'))
-                             .Select(p => new { First = int.Parse(p.First()), Last = int.Parse(p.Last()) })
-                             .SelectMany(x => Enumerable.Range(x.First, x.Last - x.First + 1))
-                             .OrderBy(z => z)
-                             .ToArray()
-            };
+            // copied from https://stackoverflow.com/a/37213725/260221
+            generator.Range = range.Split(',')
+                                   .Select(x => x.Split('-'))
+                                   .Select(p => new { First = int.Parse(p.First()), Last = int.Parse(p.Last()) })
+                                   .SelectMany(x => Enumerable.Range(x.First, x.Last - x.First + 1))
+                                   .OrderBy(z => z)
+                                   .ToArray();
             return true;
         }
         catch
         {
-            generator = null;
             return false;
         }
     }
