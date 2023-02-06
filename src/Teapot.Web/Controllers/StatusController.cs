596add25ff251ababfefe78b181586a6f673d0d3
Neil Boyd <NeilBoyd@gmail.com>
2023-02-06 07:36:23 +0100

    formatting

--- src/Teapot.Web/Controllers/StatusController.cs
+++ src/Teapot.Web/Controllers/StatusController.cs
@@ -4,12 +4,14 @@ using Teapot.Web.Models;
 
 namespace Teapot.Web.Controllers;
 
-public class StatusController : Controller {
+public class StatusController : Controller
+{
     public const string SLEEP_HEADER = "X-HttpStatus-Sleep";
 
     private readonly TeapotStatusCodeResults _statusCodes;
 
-    public StatusController(TeapotStatusCodeResults statusCodes) {
+    public StatusController(TeapotStatusCodeResults statusCodes)
+    {
         _statusCodes = statusCodes;
     }
 
@@ -17,7 +19,8 @@ public class StatusController : Controller {
     public IActionResult Index() => View(_statusCodes);
 
     [Route("{statusCode:int}", Name = "StatusCode")]
-    public IActionResult StatusCode(int statusCode, int? sleep = 0) {
+    public IActionResult StatusCode(int statusCode, int? sleep = 0)
+    {
         var statusData = _statusCodes.ContainsKey(statusCode)
             ? _statusCodes[statusCode]
             : new TeapotStatusCodeResult { Description = "Unknown Code" };
@@ -38,10 +41,13 @@ public class StatusController : Controller {
         return new StatusCodeResult((int)HttpStatusCode.BadRequest);
     }
 
-    private int? FindSleepInHeader() {
-        if (HttpContext.Request.Headers.TryGetValue(SLEEP_HEADER, out var sleepHeader) && sleepHeader.Count == 1 && sleepHeader[0] is not null) {
+    private int? FindSleepInHeader()
+    {
+        if (HttpContext.Request.Headers.TryGetValue(SLEEP_HEADER, out var sleepHeader) && sleepHeader.Count == 1 && sleepHeader[0] is not null)
+        {
             var val = sleepHeader[0];
-            if (int.TryParse(val, out var sleepFromHeader)) {
+            if (int.TryParse(val, out var sleepFromHeader))
+            {
                 return sleepFromHeader;
             }
         }
